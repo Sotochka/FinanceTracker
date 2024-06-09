6517efce2045b9427e8fc1ba470a96396c9ffaba
Oleksandr <01-23-141.stud@vntu.vn.ua>
2024-06-09 14:59:00 +0300

    Removed appsetting files

--- FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
+++ FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c51ad09fabe7a319a6674b6e5e2a1252a2578b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d7dc55ce40364568444388e5b713bb3324de35")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
